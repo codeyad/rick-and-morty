c59cef1232852f2284597663e8f4eaf1f34ae299
Melvis Yadel Perez <ing.yadperez@gmail.com>
2022-11-04 20:32:23 -0400

    Add sass modules and more styles to profile

--- src/pages/characterProfile.tsx
+++ src/pages/characterProfile.tsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { getCharacterProfile } from "../services/api";
 import { useParams } from "react-router-dom";
-
+import Style from "./characterProfile.module.scss";
+import Arrow from "../assets/images/arrow_back.png";
 const CharacterProfile = () => {
   let params: any = useParams() ?? 0;
   const [character, setCharacter] = useState<Character | null>(null);
@@ -18,13 +19,17 @@ const CharacterProfile = () => {
   return (
     <>
       {character ? (
-        <div>
-          <img src={character.image} alt='' />
-          <h1>{character.name}</h1>
-          <section id='informations'>
+        <div className={Style.container}>
+          <div className={Style.back}>
+            <img src={Arrow} alt='' />
+            <h3>GO BACK</h3>
+          </div>
+          <img className={Style.img} src={character.image} alt='' />
+          <h1 className={Style.h1}>{character.name}</h1>
+          <section className={Style.section} id='informations'>
             <h3>Informations</h3>
           </section>
-          <section id='episodes'>
+          <section className={Style.section} id='episodes'>
             <h3>Episodes</h3>
           </section>
         </div>
